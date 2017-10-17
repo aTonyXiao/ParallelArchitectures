a4074894f46437a2903ec4ad9b02f5117765c5e6
aTonyXiao <tony.xiao95@icloud.com>
2017-10-17 16:44:43 -0700

    update outline

--- OutlineofR.R
+++ OutlineofR.R
@@ -1,20 +1,39 @@
 library(data.table)
 library(parallel)
-nthread = 4
-count = 0
-cluster = makePSOCKcluster(rep("localhost", nthread))
-data = fread("test1.txt", sep = "\n", header = FALSE)
-as.numeric(data)
+edges = fread("twitter_combined.txt", sep = " ", header = FALSE)
 
-recippar(data)
+recippar(edges)
 
-recippar <- function (edges) {
-  sortedArray <- parApply(cluster,data,1,sort.int, method='quick'))#decreasing sorted
+recippar <- function(edges){
+  sorted <- edges[order(edges[,1],edges[,2])]#sort the data.table
+  nrow = nrow(edges)
+  count = 0
+  nthread = 4
+  cluster = makePSOCKcluster(rep("localhost", nthread))
+  chunk <- sample.int(nrow, nrow, replace = TRUE)#generate the random num for later each chunk
 
-  for(x in 1:length(data))
-    found <- binary_search(data, data[x], index=TRUE)
-    if(found)
-      count = count + 1
-
-  count / 2
 }
+ #order the data frame
+
+
+# between(new, lower, upper, incbounds=TRUE)
+
+
+
+
+# edge <- split(data, as.factor(data))
+
+# recippar(data)
+#
+
+
+# recippar <- function (edges) {
+#decreasing sorted
+#
+#   for(x in 1:length(data))
+#     found <- binary_search(data, data[x], index=TRUE)
+#     if(found)
+#       count = count + 1
+#
+#   count / 2
+# }
