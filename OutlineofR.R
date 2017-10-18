1f1db6d5dd5e3ea713bbbc7737bf40ea55c3c268
Justin Jia <justin.jia.developer@gmail.com>
2017-10-17 18:11:35 -0700

    Add serial version

--- OutlineofR.R
+++ OutlineofR.R
@@ -1,39 +1,23 @@
 library(data.table)
 library(parallel)
+
 edges = fread("twitter_combined.txt", sep = " ", header = FALSE)
 
-recippar(edges)
-
-recippar <- function(edges){
-  sorted <- edges[order(edges[,1],edges[,2])]#sort the data.table
+recippar <- function(edges) {
   nrow = nrow(edges)
-  count = 0
   nthread = 4
-  cluster = makePSOCKcluster(rep("localhost", nthread))
-  chunk <- sample.int(nrow, nrow, replace = TRUE)#generate the random num for later each chunk
-
+  edges = edges[order(edges[,1], edges[,2])] # sort the data.table
+  # cluster = makePSOCKcluster(rep("localhost", nthread))
+  # chunk <- sample.int(nrow, nrow, replace = TRUE) # generate the random num for later each chunk
+  count = 0
+  for (i in 1:nrow(edges)) {
+    found = edges[V1==edges[i]$V2 & V2==edges[i]$V1]
+    if (nrow(found) > 0) {
+      count = count + 1
+    }
+  }
+  count
 }
- #order the data frame
 
-
-# between(new, lower, upper, incbounds=TRUE)
-
-
-
-
-# edge <- split(data, as.factor(data))
-
-# recippar(data)
-#
-
-
-# recippar <- function (edges) {
-#decreasing sorted
-#
-#   for(x in 1:length(data))
-#     found <- binary_search(data, data[x], index=TRUE)
-#     if(found)
-#       count = count + 1
-#
-#   count / 2
-# }
+count = recippar(edges) / 2
+print(count)
