b09ff407455fc4a8b4ab6d995ff47bbe0ffa0ab4
aTonyXiao <tony.xiao95@icloud.com>
2017-10-17 18:34:53 -0700

    add parallel

--- OutlineofR.R
+++ OutlineofR.R
@@ -7,17 +7,28 @@ recippar <- function(edges) {
   nrow = nrow(edges)
   nthread = 4
   edges = edges[order(edges[,1], edges[,2])] # sort the data.table
-  # cluster = makePSOCKcluster(rep("localhost", nthread))
+  cluster = makecluster(rep("localhost", nthread))
+  mgrinit(cluster)
+  clusterEvalQ(cluster, id <- myinfo$id)
   # chunk <- sample.int(nrow, nrow, replace = TRUE) # generate the random num for later each chunk
+  clusterExport(cluster, "findPair")
+  clusterEvalQ(cluster, findPair(edges))
+
+}
+
+findPair <- function(edges) {
+  require(parallel)
+  myedges <- splitIndices(nrow(edges),myinfo$nwrkrs)[[myinfo$id]]
   count = 0
-  for (i in 1:nrow(edges)) {
-    found = edges[V1==edges[i]$V2 & V2==edges[i]$V1]
+  for (i in 1:nrow(myedges)) {
+    found = myedges[V1 == myedges[i]$V2 & V2 == myedges[i]$V1]
     if (nrow(found) > 0) {
       count = count + 1
     }
   }
   count
 }
+
 
 count = recippar(edges) / 2
 print(count)
