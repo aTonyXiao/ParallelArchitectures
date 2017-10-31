1148b0525c09fab9cf487bf1b19999f51f3484e0
Justin Jia <justin.jia.developer@gmail.com>
2017-10-30 18:54:47 -0700

    Clean up

--- Homework2/Problem1.c
+++ Homework2/Problem1.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <sys/time.h>
-#include <math.h>
 #include <mpi.h>
-
-#define NROW 1768149
 
 #define MAP_SIZE 5000000
 #define ARRAY_SIZE 6
@@ -13,8 +9,6 @@
 #define MPI_TAG_INSERT 0
 #define MPI_TAG_ERASE 1
 #define MPI_TAG_COMPLETE 2
-
-int number_of_nodes, current_node_index;
 
 unsigned long combined(unsigned long first, unsigned long second) {
   return (first << 32) | second;
@@ -106,10 +100,10 @@ void insert_value_into_map(long *map, int index, unsigned long value, int max_ar
   }
 }
 
-int other_node_recippar(int *edges, int nrow) {
+int other_node_recippar(int *edges, int nrow, int number_of_nodes, int current_node_index) {
   int count = 0;
   long *map = calloc(MAP_SIZE, sizeof(long));
-  int partition = ((int)ceil((double)nrow / (double)(number_of_nodes - 1) / 2)) * 4;
+  int partition = (nrow / (number_of_nodes - 1) + 1) * 4;
   for (int i = partition * (current_node_index - 1); i < partition * current_node_index && i < nrow * 2; i += 2) {
     unsigned long second_and_first = combined(edges[i + 1], edges[i]);
     if (find_and_erase_value_in_map(map, map_index(second_and_first), second_and_first)) {
@@ -125,8 +119,7 @@ int other_node_recippar(int *edges, int nrow) {
   return -1;
 }
 
-int first_node_recippar(int *edges)
-{
+int first_node_recippar(int *edges, int number_of_nodes) {
   int count = 0;
   long *map = calloc(MAP_SIZE, sizeof(long));
   bool *finished_nodes = calloc(number_of_nodes, sizeof(bool));
@@ -159,16 +152,18 @@ int first_node_recippar(int *edges)
 }
 
 int recippar(int *edges, int nrow) {
+  int number_of_nodes, current_node_index;
   MPI_Comm_size(MPI_COMM_WORLD, &number_of_nodes);
   MPI_Comm_rank(MPI_COMM_WORLD, &current_node_index);
   if (current_node_index == 0) { // start all nodes that aren't the first node
-    return first_node_recippar(edges);
+    return first_node_recippar(edges, number_of_nodes);
   } else { // start the last node
-    return other_node_recippar(edges, nrow);
+    return other_node_recippar(edges, nrow, number_of_nodes, current_node_index);
   }
 }
 
 int main(int argc, char** argv) {
+  int NROW = 1768149;
   FILE *twitter_combined = fopen("twitter_combined.txt", "r");
   int *edges = (int *)malloc(sizeof(int) * NROW * 2);
   for (int i = 0; i < NROW * 2; i++) {
@@ -183,9 +178,7 @@ int main(int argc, char** argv) {
   end_time = MPI_Wtime();
   MPI_Barrier(MPI_COMM_WORLD);
   MPI_Finalize();
-  if (count == -1) {
-    printf("Node %d finished.\n", current_node_index);
-  } else {
+  if (count != -1) {
     printf("Count (%lfs): %d\n", end_time - start_time, count);
   }
   return 0;
