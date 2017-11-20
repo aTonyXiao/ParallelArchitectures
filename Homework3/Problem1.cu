b8bf00896086e114320d43a0a661b0e5df5648b8
aTonyXiao <tony.xiao95@icloud.com>
2017-11-20 01:02:06 -0800

    brutal force

--- Homework3/Problem1.cu
+++ Homework3/Problem1.cu
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <cuda.h>
 
-
 __device__ float array_mean(float *x, int n) {
-  // printf("x[0]: %f\n", x[0]);
   float sum = 0;
   for (int i = 0; i < n; i++) {
     sum += x[i];
@@ -11,44 +9,47 @@ __device__ float array_mean(float *x, int n) {
   return sum / n;
 }
 
-
-__global__ void block_burst(float *x, int n, int k, int *result) {
-  cublasHandle_t handle
-  int start = blockIdx.x;
-  int length = threadIdx.x + k;
-  result[2] = 0;
-  int padding = 0;
+__global__ void block_burst(float *x, int n, int k, long *meanIdx, float * meanData) {
+  int start = threadIdx.x * blockDim.x + threadIdx.y;
+  int length = threadIdx.x * blockDim.x + threadIdx.y + k;
   extern __shared__ float currentmax[];
   currentmax[threadIdx.x] = array_mean(x + start, length);
-  int Maxidx;
-  // long resultIdx = mall
-  printf("currentmax: %f\n", currentmax);
-  __syncthreads();
-
-  cublasStatus_t cublasIsamax(handle, n, currentmax, 0, &Maxidx);
-
-
-  // if (currentmax > result[2]) {
-  //   result[0] = start;
-  //   result[1] = start + length - 1;
-  //   result[2] = currentmax;
-  //   printf("result[2]: %f\n", result[2]);
-  // }
+  meanIdx[threadIdx.x] = (x + start) << 32 | length;
+  meanData[threadIdx.x] = currentmax[threadIdx.x];
 }
 
-
 void maxburst(float *x, int n, int k, int *startend, float *bigmax) {
+  cublasHandle_t handle;
   float *device_x;
+  int maxIdx = 0;
   cudaMalloc((void **)&device_x, sizeof(float) * n);
   cudaMemcpy(device_x, x, sizeof(float) * n, cudaMemcpyHostToDevice);
-  int *device_result; // extern __shared__ int device_result[];
+  int *device_result;
   cudaMalloc((void **)&device_result, sizeof(int) * 3);
+
+  long* meanIdx;
+  cudaMalloc((void **)&meanIdx, sizeof(long) * n);// size change
+
+  float *meanData;
+  cudaMalloc((void **)&meanData, sizeof(float) * n);// size
+
   dim3 dimGrid(n - k + 1, 1);
   dim3 dimBlock(n, 1, 1);
-  block_burst<<<dimGrid, dimBlock, (n - k + 1) * n>>>(device_x, n, k, device_result);
-  cudaMemcpy(startend, device_result, sizeof(int) * 2, cudaMemcpyDeviceToHost);
-  cudaMemcpy(bigmax, device_result + 2, sizeof(int) * 1, cudaMemcpyDeviceToHost);
+
+  block_burst<<<dimGrid, dimBlock, (n - k + 1) * n>>>(device_x, n, k, meanIdx, meanData);
+
+  cudaMemcpy()
+  // cudaMemcpy(startend, device_result, sizeof(int) * 2, cudaMemcpyDeviceToHost);
+  // cudaMemcpy(bigmax, device_result + 2, sizeof(int) * 1, cudaMemcpyDeviceToHost);
+  cudaMemcpy();
+  cudaMemcpy();
+
   cudaThreadSynchronize();
+  cublasStatus_t cublasIsamax(handle, n, meanData, 0, &maxIdx);
+  long index = meanIdx[maxIdx];
+  startend[1] = index & 0xffffffff;
+  startend[0] = index >> 32;
+  *bigmax = meanData[maxIdx];
   cudaFree(device_x);
   cudaFree(device_result);
 }
